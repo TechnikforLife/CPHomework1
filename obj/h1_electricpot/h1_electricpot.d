350dc49814ca7de96f74016a197cb2ec6abeea6e
Flogie123 <flogie97@gmail.com>
2020-05-01 16:03:28 -0700

    Starting to implement H3

--- obj/h1_electricpot/h1_electricpot.d
+++
@@ -1,3 +0,0 @@
-obj/h1_electricpot/h1_electricpot.d obj/h1_electricpot/h1_electricpot.o: \
- src/h1_electricpot/h1_electricpot.c src/h1_electricpot/h1_electricpot.h \
- src/h1_electricpot/../integrate_romberg/integrate_romberg.h
--- obj/main/cphomework1.d
+++
@@ -1,4 +0,0 @@
-obj/main/cphomework1.d obj/main/cphomework1.o: src/main/cphomework1.c \
- src/main/../generalmemorymanager/generalmemorymanager.h \
- src/main/../integrate_romberg/integrate_romberg.h \
- src/main/../h1_electricpot/h1_electricpot.h
