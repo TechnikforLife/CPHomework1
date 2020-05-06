81805c6b34e26df7bd62628664b5d1f1ea1f0057
TechnikforLife <nico.dichter@gmail.com>
2020-05-06 14:12:38 +0200

    Plotter commented

--- pyth/simpleplotter.py
+++ pyth/simpleplotter.py
@@ -21,7 +21,7 @@ for i in range(2,np.shape(rawdata)[1]):
 y_fit=1/x_dat
 plt.plot(x_dat, y_fit,label="1/z",linewidth=0.5)
 
-#At essential information to the plot
+#Add essential information to the plot
 plt.legend(loc='best',ncol=2)
 plt.xlabel("z")
 plt.ylabel("V(z)")
@@ -33,6 +33,7 @@ plt.close()
 
 #***Part 2***
 
+#Load "data/number21.dat". The columns get plotted in different colors
 rawdata = np.loadtxt("data/number21.dat",delimiter=";" ,dtype=float)
 x_dat=rawdata[1:, 0]
 y_dat=rawdata[1:, 1]
@@ -40,6 +41,7 @@ for i in range(1,np.shape(rawdata)[1]):
     y_dat=rawdata[1:, i]
     plt.scatter(x_dat, y_dat,label="$a_{H2.1}=$"+str(rawdata[0,i]),s=5,marker="+",linewidth=0.5)
 
+#Load "data/number22.dat". The columns get plotted in different colors
 rawdata2 = np.loadtxt("data/number22.dat",delimiter=";" ,dtype=float)
 x_dat=rawdata2[1:, 0]
 y_dat=rawdata2[1:, 1]
@@ -47,17 +49,20 @@ for i in range(1,np.shape(rawdata)[1]):
     y_dat=rawdata2[1:, i]
     plt.scatter(x_dat, y_dat,label="$a_{H2.2}=$"+str(rawdata2[0,i]),s=5,marker="x",linewidth=0.5)
 
-
+#Add essential information to the plot
 plt.legend(loc='best',ncol=2)
 plt.xlabel("z")
 plt.ylabel("E(z)")
 plt.yscale("log")
 
+#Save the plot
 plt.savefig("data/number2.pdf", bbox_inches=0,format='pdf')
 plt.close()
 
 #***Part 3***
 
+#Load "data/number31.dat" and plot the first column differently
+#to make it more visible. The other columns get plotted in different colors
 rawdata = np.loadtxt("data/number31.dat",delimiter=";" ,dtype=float)
 x_dat=rawdata[1:, 0]
 y_dat=rawdata[1:, 1]
@@ -66,14 +71,17 @@ for i in range(2,np.shape(rawdata)[1]):
     y_dat=rawdata[1:, i]
     plt.scatter(x_dat, y_dat,label="$a_{2}=$"+str(rawdata[0,i]),s=5,marker="+",linewidth=0.5)
 
-
+#Add essential information to the plot
 plt.legend(loc='best',ncol=2)
 plt.xlabel("z")
 plt.ylabel("V(z)")
 
+#Save the plot
 plt.savefig("data/number31.pdf", bbox_inches=0,format='pdf')
 plt.close()
 
+#Load "data/number32.dat" and plot the first column differently
+#to make it more visible. The other columns get plotted in different colors
 rawdata = np.loadtxt("data/number32.dat",delimiter=";" ,dtype=float)
 x_dat=rawdata[1:, 0]
 y_dat=rawdata[1:, 1]
@@ -83,27 +91,26 @@ for i in range(2,np.shape(rawdata)[1]):
     y_dat=rawdata[1:, i]
     plt.scatter(x_dat, y_dat,label="$a_{2}=$"+str(rawdata[0,i]),s=5,marker="+",linewidth=0.5)
 
-a=[5.745382e+00]
-b=[0.1]
-plt.axvline(x=3.462772,ymin=-1,ymax=1,linewidth=0.5,label="abc")
-
+#Add essential information to the plot
 plt.legend(loc='best',ncol=2)
 plt.xlabel("z")
 plt.ylabel("E(z)")
 
+#Save the plot
 plt.savefig("data/number32.pdf", bbox_inches=0,format='pdf')
 plt.close()
 
+#Load "data/number22.dat". Just 1 column needs to be plotted.
 rawdata = np.loadtxt("data/number33.dat",delimiter=";" ,dtype=float)
 x_dat=rawdata[:, 0]
 y_dat=rawdata[:, 1]
-
 plt.scatter(x_dat, y_dat,label="Location of roots",s=5,marker="x",linewidth=0.5)
 
-
+#Add essential information to the plot
 plt.legend(loc='best',ncol=2)
 plt.xlabel("$a_2$")
 plt.ylabel("z")
 
+#Save the plot
 plt.savefig("data/number33.pdf", bbox_inches=0,format='pdf')
 plt.close()
